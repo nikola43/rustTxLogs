e04724cb43f3bea7d0a25ec7464a52f7686c608f
nkt <pauloxti@gmail.com>
2022-06-21 21:46:39 +0100

    check txg

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,20 @@ struct TX {
     block: String,
     tx_hash: String,
     from: String,
-    to: String,
+    created_nodes: u64,
 }
 
 impl std::fmt::Display for TX {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
-            "block': {}'
+            "
+            block: '{}'
             tx_hash: '{}'
             from: '{}'
-            to: '{}'",
-            self.block, self.tx_hash, self.from, self.from
+            created_nodes: '{}'
+            ",
+            self.block, self.tx_hash, self.from, self.created_nodes
         )
     }
 }
@@ -72,8 +74,8 @@ async fn main() -> web3::Result<()> {
     let rpc_url = "https://api.avax.network/ext/bc/C/rpc";
     let web3httpclient = web3::transports::Http::new(rpc_url).unwrap();
     let web3s = web3::Web3::new(web3httpclient);
-    let block_counter: u64 = 9027422; // v1 deploy block
-    let block_counter_end: u64 = 9027500; // last v1 tx
+    let block_counter: u64 = 9003760; // v1 deploy block
+    let block_counter_end: u64 = 9003770; // last v1 tx
     let block_to_check = block_counter_end - block_counter;
 
     let contract_address = "0x40064CE057Fb99a5c8e34F61365cC5996E59aB57"; // PXT V1
@@ -126,10 +128,10 @@ async fn main() -> web3::Result<()> {
                     println!("CREATE NODE");
 
                     let ctx = TX {
-                        block: current_block_index.to_string(),
                         tx_hash: w3h::to_string(&transaction_hash),
                         from: w3h::to_string(&from_addr),
-                        to: w3h::to_string(&to_addr),
+                        created_nodes: 10,
+                        block: current_block_index.to_string(),
                     };
 
                     let serialized_ctx = serde_json::to_string(&ctx).unwrap();
@@ -165,7 +167,11 @@ async fn main() -> web3::Result<()> {
 
 fn get_tx_method_id(tx: Transaction) -> String {
     let mut a_string = String::from("");
+    let mut a_string2 = String::from("");
     let tx_data_input = tx.input.0.hex_display().to_string().replace(" ", "");
+    //let tx_data_input_len = tx_data_input.len();
+
+    //println!("tx_data_input_len {}", tx_data_input_len);
 
     a_string.push(tx_data_input.chars().nth(0).unwrap());
     a_string.push(tx_data_input.chars().nth(1).unwrap());
@@ -176,7 +182,18 @@ fn get_tx_method_id(tx: Transaction) -> String {
     a_string.push(tx_data_input.chars().nth(6).unwrap());
     a_string.push(tx_data_input.chars().nth(7).unwrap());
 
-    println!("{}", a_string);
+    /*
+    a_string2.push(tx_data_input.chars().nth(35).unwrap());
+    a_string2.push(tx_data_input.chars().nth(36).unwrap());
+    a_string2.push(tx_data_input.chars().nth(37).unwrap());
+    a_string2.push(tx_data_input.chars().nth(38).unwrap());
+    a_string2.push(tx_data_input.chars().nth(39).unwrap());
+    a_string2.push(tx_data_input.chars().nth(40).unwrap());
+    */
+
+    //println!("tx_data_input_len {}", tx_data_input_len);
+    println!("a_string: {}", a_string);
+    println!("a_string2: {}", a_string2);
 
     a_string
 }
